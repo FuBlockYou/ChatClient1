89df0cfe1682dd7ee9093552b0d21babba0639fd
FuBlockYou <980709500@qq.com>
2018-07-03 17:21:20 +0800

    初始版本

--- src/com/chat/ChatClient.java
+++ src/com/chat/ChatClient.java
@@ -1,17 +1,21 @@
 package com.chat;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.*;
+import java.net.Socket;
 
 public class ChatClient extends Frame {
-
+    Socket socket = null;
+    DataOutputStream dos = null;
     TextField textField = new TextField();
     TextArea textArea = new TextArea();
 
     public static void main(String[] str) {
         new ChatClient().launchFrame();
-
     }
 
     public void launchFrame(){
@@ -25,11 +29,71 @@ public class ChatClient extends Frame {
             //采用匿名内部类实现
             @Override
             public void windowClosing(WindowEvent e) {
+                disconnect();
                 System.exit(0);
+
             }
         });
+        textField.addActionListener(new TFListener());
         setVisible(true);
+        connect();
     }
 
+    /**
+     * 链接服务器端的方法
+     */
+    private void connect() {
+        try {
+            socket = new Socket("127.0.0.1", 8888);
+            //初始化DataOutputStream
+            dos = new DataOutputStream(socket.getOutputStream());
+            //测试语句
+            System.out.println("链接成功!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+    }
+
+    /**
+     * textField监听器
+     * 当在textField中输入内容完毕后，敲击回车即可将内容显示在textArea中
+     */
+    private class TFListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String str = textField.getText().trim();
+            textArea.setText(str);
+            textField.setText("");
+            //网络发字符串
+            sendMessage(str);
+        }
+    }
+
+    /**
+     * 网络发字符串
+     */
+    private void sendMessage(String s) {
+        try {
+            dos.writeUTF(s);
+            dos.flush();
+            //dos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 关闭链接
+     */
+    private void disconnect() {
+        try {
+            dos.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
---
+++ src/com/chat/ChatServe.java
@@ -0,0 +1,66 @@
+package com.chat;
+
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.BindException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ChatServe {
+    /**
+     * 基于TCP的聊天的服务端
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        //服务启动前，标志位为false，启动之后为true
+        boolean started = false;
+        //链接标志位，如果链接成功则该标志位变为true
+        boolean bConnected = false;
+        ServerSocket serverSocket = null;
+        Socket s = null;
+        DataInputStream dis = null;
+
+        try {
+            serverSocket = new ServerSocket(8888);
+        } catch (BindException e) {
+            System.out.println("端口使用中......");
+            System.out.println("请关闭相关程序,并重新启动");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            started = true;
+
+            while (started) {
+                s = serverSocket.accept();
+                //调试语句
+                System.out.println("a client connnected");
+
+                bConnected = true;
+
+                dis = new DataInputStream(s.getInputStream());
+
+                while (bConnected) {
+                    String str = dis.readUTF();
+                    System.out.println(str);
+                }
+//                dis.close();
+            }
+        } catch (IOException e) {
+//            e.printStackTrace();
+            System.out.println("客户端已关闭");
+        } finally {
+            try {
+                if (dis != null)
+                    dis.close();
+                if (s != null)
+                    s.close();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+}
