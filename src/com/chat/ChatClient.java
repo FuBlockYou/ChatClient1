9b5563c41c84872180e3ee3e20098227ad92bd2e
FuBlockYou <980709500@qq.com>
2018-07-03 14:28:09 +0800

    窗口的实现和关闭窗口的实现

--- src/com/chat/ChatClient.java
+++ src/com/chat/ChatClient.java
@@ -1,16 +1,33 @@
 package com.chat;
 
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class ChatClient extends Frame {
 
+    TextField textField = new TextField();
+    TextArea textArea = new TextArea();
+
     public static void main(String[] str) {
         new ChatClient().launchFrame();
+
     }
 
     public void launchFrame(){
         setLocation(400,300);
         this.setSize(300,300);
+        add(textField, BorderLayout.SOUTH);
+        add(textArea, BorderLayout.NORTH);
+        pack();
+        this.addWindowListener(new WindowAdapter() {
+            //关闭窗口操作实现
+            //采用匿名内部类实现
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
         setVisible(true);
     }
 
